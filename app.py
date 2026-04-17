0ea96f929088b3192b6305fdf4ea73f8a25c614e
xiyue-yeah <ladiba12_8@hotmail.com>
2026-04-17 20:54:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        self.wfile.write("<h1>Hello！服务器全自动部署成功啦！</h1>".encode('utf-8'))
+        self.wfile.write("<h1>Hello！这是 team1 服务</h1>".encode('utf-8'))
 
 if __name__ == '__main__':
     server = HTTPServer(('0.0.0.0', 8001), RequestHandler)
